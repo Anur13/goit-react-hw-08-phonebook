0a465a6c367af1a9915b13a0025a4ddd59560a07
Anur13 <akrupskyi13@gmail.com>
2021-03-24 16:51:17 +0200

    fin

--- src/Redux/phonebook/phonebook-reducer.js
+++ src/Redux/phonebook/phonebook-reducer.js
@@ -1,77 +1,101 @@
-import { createReducer } from "@reduxjs/toolkit"
+import { createReducer } from '@reduxjs/toolkit';
 import {
-    submit,
-    deleteItem,
-    submitContactRequest,
-    submitContactSuccess,
-    submitContactError,
-    submitdDeleteItemRequest,
-    submitDeleteItemSuccess,
-    submitDeleteItemError,
-    getContacts,
-    submitGetItemsRequest,
-    submitGetItemsSuccess,
-    submitGetItemsError,
-    submitSignUpRequest,
-    submitSignUpSuccess,
-    submitSignUpError,
-    submitLoginRequest,
-    submitLoginSuccess,
-    submitLoginsError,
-    submitLogOutRequest,
-    submitLogOutSuccess,
-    submitLogOutsError,
-    submitGetCurrentUserRequest,
-    submitGetCurrentUserSuccess,
-    submitGetCurrentUsesError,
-} from "./phonebook-actions"
+  submit,
+  deleteItem,
+  submitContactRequest,
+  submitContactSuccess,
+  submitContactError,
+  submitdDeleteItemRequest,
+  submitDeleteItemSuccess,
+  submitDeleteItemError,
+  getContacts,
+  submitGetItemsRequest,
+  submitGetItemsSuccess,
+  submitGetItemsError,
+  submitSignUpRequest,
+  submitSignUpSuccess,
+  submitSignUpError,
+  submitLoginRequest,
+  submitLoginSuccess,
+  submitLoginsError,
+  submitLogOutRequest,
+  submitLogOutSuccess,
+  submitLogOutsError,
+  submitGetCurrentUserRequest,
+  submitGetCurrentUserSuccess,
+  submitGetCurrentUsesError,
+} from './phonebook-actions';
 
 const phonebookReducer = createReducer([], {
-    [submitContactSuccess]: (state, { payload }) => [...state, payload],
-    [submitDeleteItemSuccess]: (state, { payload }) => state.filter((item) => item.id !== payload),
-    [submitGetItemsSuccess]: (state, { payload }) => payload,
-})
+  [submitContactSuccess]: (state, { payload }) => [...state, payload],
+  [submitDeleteItemSuccess]: (state, { payload }) =>
+    state.filter(item => item.id !== payload),
+  [submitGetItemsSuccess]: (state, { payload }) => payload,
+});
 
 const loginisationReducer = createReducer(false, {
-    [submitLoginSuccess]: () => true,
-    [submitSignUpSuccess]: () => true,
-    [submitLogOutSuccess]: () => false,
-    [submitGetCurrentUserSuccess]: () => true,
-    [submitGetCurrentUsesError]: () => false,
-})
+  [submitLoginSuccess]: () => true,
+  [submitSignUpSuccess]: () => true,
+  [submitLogOutSuccess]: () => false,
+  [submitGetCurrentUserSuccess]: () => true,
+  [submitGetCurrentUsesError]: () => false,
+});
 
-const tokenReducer = createReducer("", {
-    [submitSignUpSuccess]: (state, { payload }) => payload.token,
-    [submitLoginSuccess]: (state, { payload }) => payload,
-    [submitLogOutSuccess]: () => "",
-})
+const tokenReducer = createReducer('', {
+  [submitSignUpSuccess]: (state, { payload }) => payload.token,
+  [submitLoginSuccess]: (state, { payload }) => payload,
+  [submitLogOutSuccess]: () => '',
+});
 
-const emailReducer = createReducer("", {
-    [submitLoginSuccess]: (state, { payload }) => payload.user.email,
-    [submitSignUpSuccess]: (state, { payload }) => payload.user.email,
-    [submitLogOutSuccess]: () => "",
-})
+const emailReducer = createReducer('', {
+  [submitLoginSuccess]: (state, { payload }) => payload.user.email,
+  [submitSignUpSuccess]: (state, { payload }) => payload.user.email,
+  [submitLogOutSuccess]: () => '',
+});
 
 const loaderReducer = createReducer(false, {
-    [submitContactRequest]: () => true,
-    [submitContactSuccess]: () => false,
-    [submitContactError]: () => false,
-    [submitdDeleteItemRequest]: () => true,
-    [submitDeleteItemSuccess]: () => false,
-    [submitDeleteItemError]: () => false,
-    [getContacts]: () => true,
-    [submitGetItemsRequest]: () => true,
-    [submitGetItemsSuccess]: () => false,
-    [submitGetItemsError]: () => false,
-})
+  [submitContactRequest]: () => true,
+  [submitContactSuccess]: () => false,
+  [submitContactError]: () => false,
+  [submitdDeleteItemRequest]: () => true,
+  [submitDeleteItemSuccess]: () => false,
+  [submitDeleteItemError]: () => false,
+  [getContacts]: () => true,
+  [submitGetItemsRequest]: () => true,
+  [submitGetItemsSuccess]: () => false,
+  [submitGetItemsError]: () => false,
 
-const errorReducer = createReducer("", {
-    [submitLoginsError]: (state, { payload }) => payload,
-    [submitDeleteItemError]: (state, { payload }) => payload,
-    [submitGetItemsError]: (state, { payload }) => payload,
-    [submitSignUpError]: (state, { payload }) => payload,
-    [submitLogOutsError]: (state, { payload }) => payload,
-    [submitGetCurrentUsesError]: (state, { payload }) => payload,
-    [submitContactError]: (state, { payload }) => payload,
-})
-export { phonebookReducer, loaderReducer, loginisationReducer, tokenReducer, emailReducer, errorReducer }
+  [submitSignUpRequest]: () => true,
+  [submitSignUpSuccess]: () => false,
+  [submitSignUpError]: () => false,
+
+  [submitLoginRequest]: () => true,
+  [submitLoginSuccess]: () => false,
+  [submitLoginsError]: () => false,
+
+  [submitLogOutRequest]: () => true,
+  [submitLogOutSuccess]: () => false,
+  [submitLogOutsError]: () => false,
+
+  [submitGetCurrentUserRequest]: () => true,
+  [submitGetCurrentUserSuccess]: () => false,
+  [submitGetCurrentUsesError]: () => false,
+});
+
+const errorReducer = createReducer('', {
+  [submitLoginsError]: (state, { payload }) => payload,
+  [submitDeleteItemError]: (state, { payload }) => payload,
+  [submitGetItemsError]: (state, { payload }) => payload,
+  [submitSignUpError]: (state, { payload }) => payload,
+  [submitLogOutsError]: (state, { payload }) => payload,
+  [submitGetCurrentUsesError]: (state, { payload }) => payload,
+  [submitContactError]: (state, { payload }) => payload,
+});
+export {
+  phonebookReducer,
+  loaderReducer,
+  loginisationReducer,
+  tokenReducer,
+  emailReducer,
+  errorReducer,
+};
--- src/Redux/phonebook/phonebook-selectors.js
+++ src/Redux/phonebook/phonebook-selectors.js
@@ -2,7 +2,7 @@ import { createSelector } from 'reselect';
 import { useSelector } from 'react-redux';
 const getContactItem = state => state.contacts.items;
 const getContactsFilter = state => state.contacts.filter;
-const getLoading = state => state.contacts.isLoggedIn;
+const getLoading = state => state.contacts.loading;
 
 const getShownContacts = createSelector(
   [getContactItem, getContactsFilter],
